c49886255847d2a1ea78485e50975d468f15978f
EngAbdelrahman <abdelrahman.elkaliby@gmail.com>
2022-01-25 11:02:02 +0200

    agora live stream

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const String APP_ID = "0e7046f226414468a000648899d7a1d6";
+const String Token = "0060e7046f226414468a000648899d7a1d6IAAyFAbQqEllvSFqV1GCxmHcjbcg+cs/+zFC4/4N2g6o9qtapy0AAAAAEAAcdJDBMALxYQEAAQAwAvFh";
+const String channelName = "liveStream";
